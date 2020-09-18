c0c1ed1671e5af9c9bf90807a2abdd239b247fc8
Uma Chiu <sijimi777asari@gmail.com>
2020-09-18 21:41:08 +0900

    DeleteViewControllerへラベルを追加 ラベルの内容を変更した

--- CountApp/Controller/DeleteViewController.swift
+++ CountApp/Controller/DeleteViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class DeleteViewController: UIViewController {
     
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var message2Label: UILabel!
+    
+    
+    //カラー定数宣言
+    let backGroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    let buttonRGBA = UIColor(red: 255/255.0, green: 126/255.0, blue: 121/255.0, alpha: 1.0)
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //背景色設定
+        self.view.backgroundColor = backGroundColor
+        
+        /*-- ラベルデザイン --*/
+        //最初からやり直したいとき
+        messageLabel.frame = CGRect(x: view.frame.size.width / 5, y: view.frame.size.height / 3    , width: view.frame.size.width / 1.67, height: view.frame.size.height / 17)
+        //削除後はもとに戻せません
+        message2Label.frame = CGRect(x: view.frame.size.width / 5, y: view.frame.size.height / 2.5    , width: view.frame.size.width / 1.67, height: view.frame.size.height / 17)
+        
+        /*-- データ削除 ボタンデザイン --*/
+        deleteButton.layer.borderColor = buttonRGBA.cgColor
+        deleteButton.layer.borderWidth = 1
+        deleteButton.layer.backgroundColor = .none
+        deleteButton.layer.cornerRadius = 5.0
+        deleteButton.frame = CGRect(x: view.frame.size.width / 5, y: view.frame.size.height / 1.8    , width: view.frame.size.width / 1.67, height: view.frame.size.height / 17)
+
 
         // Do any additional setup after loading the view.
     }
