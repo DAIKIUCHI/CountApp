543b4af2e75cab18969dc103f0a9c92d449fbe9a
Uma Chiu <sijimi777asari@gmail.com>
2020-09-19 10:55:13 +0900

    About画面のレイアウト変更 コードからオートレイアウトへ変更

--- CountApp/Controller/AboutViewController.swift
+++ CountApp/Controller/AboutViewController.swift
@@ -12,11 +12,15 @@ class AboutViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var text1: UILabel!
-    @IBOutlet weak var text2: UITextView!
+//    @IBOutlet weak var text2: UITextView!
+    @IBOutlet weak var text2: UILabel!
     @IBOutlet weak var text3: UILabel!
-    @IBOutlet weak var text4: UITextView!
+//    @IBOutlet weak var text4: UITextView!
+    @IBOutlet weak var text4: UILabel!
+    @IBOutlet weak var text4_2: UILabel!
     @IBOutlet weak var text5: UILabel!
-    @IBOutlet weak var text6: UITextView!
+//    @IBOutlet weak var text6: UITextView!
+    @IBOutlet weak var text6: UILabel!
     @IBOutlet weak var text7: UILabel!
     @IBOutlet weak var text8: UILabel!
     @IBOutlet weak var twitterLinkLabel: UIButton!
@@ -26,66 +30,8 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        textLayout()
 
         // Do any additional setup after loading the view.
-    }
-    
-    func textLayout() {
-        //テキストラベルの位置、サイズ
-        titleLabel.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 9, width: 200, height: 50)
-        
-//        titleLabel.layer.borderWidth = 2.0
-//        titleLabel.layer.borderColor = rgba.cgColor
-        
-        //テキストラベルの位置、サイズ
-        text1.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 6.7, width: 300, height: 50)
-        
-//        text1.layer.borderWidth = 2.0
-//        text1.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text2.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 4.5, width: 300, height: 83)
-        
-//        text2.layer.borderWidth = 2.0
-//        text2.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text3.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 2.8, width: 170, height: view.frame.size.height / 30)
-        
-//        text3.layer.borderWidth = 2.0
-//        text3.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text4.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 2.6, width: 330, height: 150)
-        
-//        text4.layer.borderWidth = 2.0
-//        text4.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text5.frame = CGRect(x: view.frame.size.width / 13, y: view.frame.size.height / 1.65, width: 300, height: 30)
-        
-//        text5.layer.borderWidth = 2.0
-//        text5.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text6.frame = CGRect(x: view.frame.size.width / 12, y: view.frame.size.height / 1.55, width: 340, height: 50)
-        
-//        text6.layer.borderWidth = 2.0
-//        text6.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text7.frame = CGRect(x: view.frame.size.width / 13, y: view.frame.size.height / 1.4, width: 300, height: 30)
-        
-//        text7.layer.borderWidth = 2.0
-//        text7.layer.borderColor = rgba.cgColor
-
-        //テキストラベルの位置、サイズ
-        text8.frame = CGRect(x: view.frame.size.width / 11, y: view.frame.size.height / 1.3, width: 300, height: 20)
-
-        //テキストラベルの位置、サイズ
-        twitterLinkLabel.frame = CGRect(x: view.frame.size.width / 1.6, y: view.frame.size.height / 1.2, width: 100, height: 30)
     }
     
     
