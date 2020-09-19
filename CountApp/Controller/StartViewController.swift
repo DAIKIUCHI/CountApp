d34480810741248c1bf8f1d8278f590bc61bc37d
Uma Chiu <sijimi777asari@gmail.com>
2020-09-19 13:58:11 +0900

    初回画面レイアウトを変更 オートレイアウト、テキスト内容変更、イメージ追加

--- CountApp/Controller/StartViewController.swift
+++ CountApp/Controller/StartViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var rgba = UIColor(red: 255/255.0, green: 126/255.0, blue: 121/255.0, alpha: 1.0)
+    var penetrateRGBA = UIColor(red: 255/255.0, green: 126/255.0, blue: 121/255.0, alpha: 0.3)
+    var backgroundRGBA = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //背景色設定
+        self.view.backgroundColor = backgroundRGBA
+        
+        /*-- 次へボタン --*/
+        //角丸
+        startButton.layer.cornerRadius = 5.0
+        //ボタンの背景色
+        startButton.layer.backgroundColor = rgba.cgColor
+        //ボタンのテキストカラー
+        startButton.setTitleColor(.white, for: .normal)
+        
     }
     
 
